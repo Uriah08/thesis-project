a4dc0d2a20794e55379ef41e12e2a772c322f6d5
Uriah08 <lorenz08.flores@gmail.com>
2025-05-07 22:11:52 +0800

    Authentication Started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className='h-full w-full flex justify-center'>
-        <div className='max-w-[1200px] h-12 w-full'></div>
+        <div className='max-w-[1200px] h-screen w-full flex items-center justify-center'>
+          <Button>Sign In</Button>
+        </div>
     </div>
   );
 }
